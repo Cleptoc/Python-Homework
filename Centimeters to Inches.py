4747189061ae731d3cd8f6411c692b01fe7ca56a
Andy <and.paul7121@gmail.com>
2017-12-05 17:21:02 -0800

    Add files via upload

---
+++ Centimeters to Inches.py
@@ -0,0 +1,7 @@
+# Python Program to convert measurement in Centimeters to inches
+Centimeters = float(input("Input measurement in centimeters: "))
+# convert to inches
+Inches = Centimeters / 2.54
+# show results
+print(Centimeters, "Centimeters equals", Inches, "Inches")
+
