89d46c1347a27f7726814ccefbb7beb8db3e46c6
Andy <and.paul7121@gmail.com>
2017-12-05 17:21:39 -0800

    Add files via upload

---
+++ Farenheit to Celsius .py
@@ -0,0 +1,9 @@
+# Python Program to convert temperature in celsius to fahrenheit
+Celsius = float(input("Input temperature in Farenheit: "))
+# calculate fahrenheit
+Farenheit = Celsius + 32
+Farenheit = Celsius * 1.8
+# show results
+print(Celsius, "Celsius equals", Farenheit, "Farenheit")
+
+
